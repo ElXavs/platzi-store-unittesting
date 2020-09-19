52a4ad95e17337c426584d191e0deb65fa7da601
Xavier Garcia <xaviergn60@gmail.com>
2020-09-19 11:23:52 -0600

    clase 10 terminada

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
+//para utilizar los snapshots debemos convertir el componente a un json, para eso esta react-test-renderer
+import { create } from 'react-test-renderer';
 
 import Footer from '../../components/Footer';
 
@@ -12,5 +14,16 @@ describe('<Footer/>', () => {
   //esto checha que el texto del elemento sea equivalente a
   test('Render del titulo', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+//pruebas de snapshot
+//los snapshots nos permiten comprobar la ui y establecer una regla cuando tenemos componentes que no tienen otra
+//funcion mas que la visual de ser presentados en pantalla
+//comprobar que la ui siempre es la misma
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
